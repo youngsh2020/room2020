7e8a371dd376565543e0a1884167c3b5ad514f9d
youngsh2020 <60148135+youngsh2020@users.noreply.github.com>
2020-02-15 15:45:11 -0500

    Update PasswordValidator.java

--- src/midtermreviewcodeforpartc/PasswordValidator.java
+++ src/midtermreviewcodeforpartc/PasswordValidator.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Advantages: In future whenever we need to update the rulese for password validation
+ 
  */
 package midtermreviewcodeforpartc;
 
