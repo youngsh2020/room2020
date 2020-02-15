7d294854c33092e45f46c78cf0e90a9188a4839d
Youngshin <Youngshin@10.16.16.59>
2020-02-15 15:29:31 -0500

    last commit

---
+++ src/midtermreviewcodeforpartc/PasswordValidator.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package midtermreviewcodeforpartc;
+
+/**
+ *
+ * @author Youngshin
+ */
+public class PasswordValidator {
+    public boolean validatePassword(String password){
+        boolean flag = false;
+        int specialCharCount=0;
+            //iterate over each character to see if it is a special character
+            for(int i=0;i<password.length(); i++)
+            {
+                if(!(Character.isLetterOrDigit(password.charAt(i))))
+                {
+                    //now we know there is at least one special character
+                    specialCharCount++;
+                }
+            }
+            if(specialCharCount>0 &&password.length()>7)
+            {
+                flag=true;
+            }
+            return flag;
+        
+    }
+    
+}
--- src/midtermreviewcodeforpartc/UnoOnline.java
+++ src/midtermreviewcodeforpartc/UnoOnline.java
@@ -51,20 +51,7 @@ public class UnoOnline
             System.out.println("Please enter your desired password:"); 
             
             password = sc.nextLine();
-            int specialCharCount=0;
-            //iterate over each character to see if it is a special character
-            for(int i=0;i<password.length(); i++)
-            {
-                if(!(Character.isLetterOrDigit(password.charAt(i))))
-                {
-                    //now we know there is at least one special character
-                    specialCharCount++;
-                }
-            }
-            if(specialCharCount>0 &&password.length()>7)
-            {
-                validPassword=true;
-            }
+            
         }//loop only ends when password is valid so now we create the User
         
         User newUser = new User(userName, password);
