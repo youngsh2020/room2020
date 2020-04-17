0492f579a8bdc97ed1255779558b8ef6947c1efa
Youngshin <Youngshin@192.168.1.11>
2020-04-16 20:41:06 -0400

    second commit

---
+++ connect.php
@@ -0,0 +1,8 @@
+<?php 
+// Youngshin Choi
+// For the safty of my information
+    $user = "chyoungs_guest";
+    $passwd = "syst10199_wk11";
+    $hostname = "localhost"; // OPTIONAL (not private data)
+   
+?>
